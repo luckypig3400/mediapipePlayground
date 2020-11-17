ed0f2fba6751fc2b98528b10ff2d7db8db864cc9
郵差 <a34007024@gmail.com>
2020-11-17 12:18:13 +0800

    fetch two python files from my Internship Log
    https://hackmd.io/@luckyOwO/1091InternshipLog

---
+++ HandTrackingControlMouse.py
@@ -0,0 +1,64 @@
+import cv2
+import mediapipe as mp
+
+# import numpy as np
+import pyautogui
+
+webcam_id = 0
+window_name = 'Hand Tracking Control Mouse Example'
+
+desktop_width, desktop_height = pyautogui.size()
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+hands = mp_hands.Hands(
+    min_detection_confidence=0.7, min_tracking_confidence=0.5)
+cap = cv2.VideoCapture(webcam_id, cv2.CAP_DSHOW)
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        break
+
+    # Flip the image horizontally for a later selfie-view display, and convert
+    # the BGR image to RGB.
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        image_rows, image_cols, _ = image.shape
+        idx_to_coordinates = {}
+        for idx, landmark in enumerate(hand_landmarks.landmark):
+            if landmark.visibility < 0 or landmark.presence < 0:
+                continue
+            landmark_px = mp.python.solutions.drawing_utils._normalized_to_pixel_coordinates(landmark.x, landmark.y,
+                                                           image_cols, image_rows)
+            if landmark_px:
+                idx_to_coordinates[idx] = landmark_px
+
+        # Screen Monitor
+        window_x, window_y, window_w, window_h = cv2.getWindowImageRect(window_name)
+        # xpos, ypos = kp_orig[4,:2]
+        xpos ,ypos = idx_to_coordinates[8]
+        pyautogui.moveTo(window_x + xpos, window_y + ypos)
+        # x = xpos / window_w * desktop_width
+        # y = ypos / window_h * desktop_height
+
+    cv2.imshow(window_name, image)
+
+    if cv2.waitKey(5) & 0xFF == 27:
+        print(idx_to_coordinates)
+        break
+
+hands.close()
+cap.release()
---
+++ tryToUnderStandMediapipeHandTracking.py
@@ -0,0 +1,72 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+from mediapipe.framework.formats import landmark_pb2
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+hand_landmarksOutputCount = 1
+desktop_width, desktop_height = pyautogui.size()
+
+myhand_landmarkList = []
+myhand_landmarkDic = {}
+# dictionary 用來包每個手部關鍵點的資訊
+# list 用來儲存及標示每個手部關鍵點的字典分別代表再圖上代表哪個點
+
+#        8   12  16  20
+#        |   |   |   |
+#        7   11  15  19
+#    4   |   |   |   |
+#    |   6   10  14  18
+#    3   |   |   |   |
+#    |   5---9---13--17
+#    2    \         /
+#     \    \       /
+#      1    \     /
+#       \    \   /
+#        ------0-
+
+# For webcam input:
+hands = mp_hands.Hands(
+    min_detection_confidence=0.8, min_tracking_confidence=0.66)
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        break
+
+    # Flip the image horizontally for a later selfie-view display, and convert
+    # the BGR image to RGB.
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            # print("第", hand_landmarksOutputCount, "次輸出landmark")
+            # print("第", hand_landmarksOutputCount, "次輸出HAND_CONNECTIONS")
+            # print(mp_hands.HAND_CONNECTIONS)
+            # hand_landmarksOutputCount += 1
+
+        for idx, landmark in enumerate(hand_landmarks.landmark[0:8]):
+            print(idx, landmark)
+
+        # mouseX = results.multi_hand_landmarks[8]['x'] * desktop_width
+        # mouseY = results.multi_hand_landmarks[8]['y'] * desktop_height
+        # print(mouseX , mouseY)
+
+        # print("len :",len(results.multi_hand_landmarks))
+        # print("type:", type(results.multi_hand_landmarks))
+
+    cv2.imshow('MediaPipe Hands', image)
+    if cv2.waitKey(5) & 0xFF == 27:
+        break
+hands.close()
+cap.release()
