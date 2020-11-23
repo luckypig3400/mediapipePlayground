386ade99efc615dfc1ad3f6b01d6cbfe01f66c24
郵差 <a34007024@gmail.com>
2020-11-23 16:05:01 +0800

    add left click funxtion

--- mpHandTrackingControlMouse.py
+++ mpHandTrackingControlMouse.py
@@ -55,6 +55,8 @@ while cap.isOpened():
         pyautogui.moveTo(indexFingerTIP_x / window_w * desktop_width, indexFingerTIP_y / window_h * desktop_height)
         # x = xpos / window_w * desktop_width
         # y = ypos / window_h * desktop_height
+        if abs(thumbTIP_x - indexFingerTIP_x) <= 6 and abs(thumbTIP_y - indexFingerTIP_y) <= 15:
+            pyautogui.leftClick()
 
     cv2.imshow(window_name, image)
 
