723182146883b5c830cebf69a19e1cd212685f7c
郵差 <a34007024@gmail.com>
2020-12-15 10:27:23 +0800

    add variables to control drawing color and line thickness

--- 【MyVersion】mpHolisticChangeDrawingColor.py
+++ 【MyVersion】mpHolisticChangeDrawingColor.py
@@ -5,6 +5,8 @@ mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 
 webcamID = 1
+landmark_drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1, color=(0, 255, 255))
+connection_drawing_spec = mp_drawing.DrawingSpec(thickness=1, color=(255, 0, 0))
 
 # For webcam input:
 holistic = mp_holistic.Holistic(
@@ -29,7 +31,7 @@ while cap.isOpened():
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     mp_drawing.draw_landmarks(
-        image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
+        image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS, landmark_drawing_spec, connection_drawing_spec)
     mp_drawing.draw_landmarks(
         image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
     mp_drawing.draw_landmarks(
