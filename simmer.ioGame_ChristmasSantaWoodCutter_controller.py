60c761416e37c4176e7b38fbde9fa44ebaffec08
郵差 <a34007024@gmail.com>
2020-12-21 23:16:37 +0800

    write outline for game controller

--- simmer.ioGame_ChristmasSantaWoodCutter_controller.py
+++ simmer.ioGame_ChristmasSantaWoodCutter_controller.py
@@ -1,6 +1,16 @@
-
-
 # for this game on i.simmer.io:Christmas Santa Wood Cutter
 # https://simmer.io/@Zathos/christmas-santa-wood-cutter
-
 # 左右手握拳，對應遊戲上砍左邊/砍右邊 偵測到握拳自動點擊遊戲上對應的控制區一下
+import os
+
+import MyVersionHandGestureDetectorUsingAngle_support2hands as twoHandGesture
+
+# os.system("MyVersionHandGestureDetectorUsingAngle_support2hands.py")
+
+if (twoHandGesture.hand1_label == "Left" and twoHandGesture.hand1GestureJudgeResult == "Zero") or (
+        twoHandGesture.hand2_label == "Left" and twoHandGesture.hand2GestureJudgeResult == "Zero"):
+    print("YA~ should send left click signal on Left side of screen")
+
+if (twoHandGesture.hand1_label == "Right" and twoHandGesture.hand1GestureJudgeResult == "Zero") or (
+        twoHandGesture.hand2_label == "Right" and twoHandGesture.hand2GestureJudgeResult == "Zero"):
+    print("YA~ should send left click signal on Right side of screen")
